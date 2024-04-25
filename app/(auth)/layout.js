7f66b71b4646370fa996f8b279814d18c0342a27
phung hoang <lnphunghoang190903@gmail.com>
2024-04-25 09:24:30 +0700

    Fix version conflict

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google"
-import "../global.css"
+import { ClerkProvider } from "@clerk/nextjs"
+
+import "../globals.css";
 
 export const metadata = {
     title: 'Auth',
@@ -10,8 +12,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
-            <body className={`${inter.className} bg-purple-1`}>{children}</body>
-        </html>
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} bg-purple-1`}>{children}</body>
+            </html>
+        </ClerkProvider>
     )
 }
--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+export default function Home() {
+    return (
+        <div className="h-screen">
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    )
 }
-
-export default Home
