08a2a2247a46dc315c0b2805eb3d6273d108bc1a
phung hoang <lnphunghoang190903@gmail.com>
2024-04-28 21:44:35 +0700

    Loading Left Bar disabled

--- components/layout/LeftSideBar.jsx
+++ components/layout/LeftSideBar.jsx
@@ -28,9 +28,10 @@ const LeftSideBar = () => {
     }
   }, [user]);
 
-  return loading || !isLoaded ? (
-    <Loader />
-  ) : (
+  // return loading || !isLoaded ? (
+  //   <Loader />
+  // ) : (
+    return (
     <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
       <Link href="/">
         <Image src="/assets/logo.png" alt="logo" width={200} height={200} />
