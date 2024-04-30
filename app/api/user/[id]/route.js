6adc9df3863f0a712b057699b263675d8175c13e
phung hoang <lnphunghoang190903@gmail.com>
2024-04-30 16:41:53 +0700

    Fixed LeftSideBar Errors

--- app/api/user/[id]/route.js
+++ app/api/user/[id]/route.js
@@ -2,23 +2,14 @@ import User from "@lib/models/User";
 import { connectToDB } from "@lib/mongodb/mongoose";
 
 export const GET = async (req, { params }) => {
-  try {
-    await connectToDB();
+    try {
+        await connectToDB()
 
-    const user = await User.findOne({ clerkId: params.id })
-      .populate({
-        path: "followers following",
-        model: User,
-        populate: {
-          path: "posts savedPosts likedPosts",
-          model: Post,
-        },
-      })
-      .exec();
+        const user = await User.findOne({ clerkId: params.id }).populate("followers following").exec()
 
-    return new Response(JSON.stringify(user), { status: 200 });
-  } catch (err) {
-    console.error(err);
-    return new Response("Failed to get user", { status: 500 });
-  }
-};
+        return new Response(JSON.stringify(user), {status: 200})
+    } catch (error) {
+        console.error(error)
+        return new Response("Failed to get user", {status: 500})
+    }
+}
--- components/layout/LeftSideBar.jsx
+++ components/layout/LeftSideBar.jsx
@@ -28,10 +28,9 @@ const LeftSideBar = () => {
     }
   }, [user]);
 
-  // return loading || !isLoaded ? (
-  //   <Loader />
-  // ) : (
-    return (
+  return loading || !isLoaded ? (
+    <Loader />
+  ) : (
     <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
       <Link href="/">
         <Image src="/assets/logo.png" alt="logo" width={200} height={200} />
@@ -39,7 +38,7 @@ const LeftSideBar = () => {
 
       <div className="flex flex-col gap-2">
         <div className="flex flex-col gap-2 items-center text-light-1">
-          <Link href={`/profile/${userData._id}/posts`}>
+          <Link href="/">
             <Image
               src={userData?.profilePhoto}
               alt="profile photo"
