27426f4a1fc208e7cc222a748648671dec5d8d8f
phung hoang <lnphunghoang190903@gmail.com>
2024-05-04 08:41:31 +0700

    Fetch Posts

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,9 +1,31 @@
-import { UserButton } from "@clerk/nextjs";
+"use client"
 
-export default function Home() {
-    return (
-        <div className="h-screen">
-            <UserButton afterSignOutUrl="/" />
-        </div>
-    )
+import Loader from '@components/Loader'
+import PostCard from '@components/cards/PostCard'
+import React, { useEffect, useState } from 'react'
+
+const Home = () => {
+    const [loading, setLoading] = useState(true)
+
+    const [feedPost, setFeedPost] = useState([])
+
+    const getFeedPost = async () => {
+        const response = await fetch("/api/post");
+        const data = await response.json();
+        setFeedPost(data);
+        setLoading(false);
+      };
+
+    useEffect(() => {
+        getFeedPost()
+    }, [])
+  return loading ? <Loader /> : (
+    <div className='flex flex-col gap-10'>
+        {feedPost.map((post) => (
+            <PostCard />
+        ))} 
+    </div>
+  )
 }
+
+export default Home
